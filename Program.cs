08cbfe82ad673b8b89b306d59b4d7bba95c0e8ca
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-25 09:56:23 +0200

    more knoledge of return keyword

--- Program.cs
+++ Program.cs
@@ -4,25 +4,20 @@ namespace _01.MethodTutorial
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            string message = Console.ReadLine();
-            //console.ReadLine(); returns a string
-            //float price = GetPrice() * quantity * 1.20 ; //can be used in expression
-            //can be used to another method
-            int age = int.Parse(Console.ReadLine()); //used into /to another method Console.ReadLine() -returns string
-                                                     //this strong is passed to method int.Parse and int.Parse returns number
-                                                     // if we want to retur a value we then dotn write "void" --we write return keyword, and we specify: 
-                                                     //the type of data that our method will return
-                                                     //
-            static int Multiply(int firstNum, int secondNum)
-            {
-                return firstNum * secondNum; //return means give us back a value;
-                                             // in return keyword it is importatn to return the same typeor compatible to  method type;
-                                             //Method type here is int -then we return a value result in  type result;
-
-                //the combination of methods name + methods parameters is called methods signature//method signature
-                //method signature = methods name + methods parameters , so we call them-methods signature;
-            }
+            Multiply(1, 5);
         }
-}
+        static int Multiply(int firstNum, int secondNum)
+        {
+
+            Console.WriteLine("Hello World!"); // here we print this 
+            Console.WriteLine(firstNum * secondNum);
+            return firstNum * secondNum; //the program-current Method block code stops here
+            Console.WriteLine("Hello World!"); //this here after return keyword is unreachable code, 
+
+            //the program down the multiply code block is not performed;
+            // return means stop give back a value result and get out of the current method;
+        }
+    }
